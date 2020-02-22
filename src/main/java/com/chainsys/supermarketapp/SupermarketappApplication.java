058a2ae5598267a97b93602509c424cd72f599ec
ChainsysTraining <naresh.20191224@gmail.com>
2020-02-22 05:40:01 +0530

    Update SupermarketappApplication.java

--- src/main/java/com/chainsys/supermarketapp/SupermarketappApplication.java
+++ src/main/java/com/chainsys/supermarketapp/SupermarketappApplication.java
@@ -4,13 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication(exclude=DataSourceAutoConfiguration.class)
 @ServletComponentScan("com.chainsys")
-public class SupermarketappApplication {
+public class SupermarketappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupermarketappApplication.class, args);
+	}
+		@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SupermarketappApplication.class);
 	}
 
 }
